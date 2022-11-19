872ddf0f09fc6342fc218423cdef88107411c1d6
GabiCMendes <gabrielacarvalhomendes2008@gmail.com>
2022-11-19 16:25:11 -0300

    projeto 41

--- js/game.js
+++ js/game.js
@@ -134,7 +134,7 @@ class Game{
                       if(obstacleGroup.isTouching(players)){
                        // gameState = 0;
                        // gameState = 1;
-                       // gameState = 2;
+                        gameState = 2;
                        // gameState = 3;
                       }
                   }
@@ -168,8 +168,8 @@ class Game{
     addObstacles()
     {       
             var x, y;
-            //x= 200;
-            //x = random(0, width-100);
+           
+            x = random(0, width-100);
             //x = random(0);
             //x = random();
             
